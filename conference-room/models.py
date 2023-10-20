d3bf49f67f984bbfc026aff7afc69dc396782a67
Devendra Pratap <dps.manit@gmail.com>
2023-10-20 22:41:34 +0530

    base setup for meeting management

---
+++ conference-room/models.py
@@ -0,0 +1,97 @@
+import json, tomllib
+
+from dataclasses import dataclass, asdict, field
+
+data = {}
+
+@dataclass
+class User:
+    name: str
+
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
+    __key__ ="users"
+    __filter__ ="name"
+
+@dataclass
+class Floor:
+    name: str
+    rooms: list[str]
+
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
+    __key__ ="floors"
+    __filter__ ="name"
+
+@dataclass
+class Building:
+    name: str
+    time_slot: list[int]
+    floors: list[Floor] = field(default=[], default_factory=list)
+
+    def __post_init__(self):
+        self.floors = get_list({Floor.__key__: self.floors}, Floor)
+
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
+    __key__ ="buildings"
+    __filter__ ="name"
+
+@dataclass
+class Meeting:
+    id: str
+    user: str
+    time_slot: list[int]
+    building: Building
+    floor: Floor
+    room: str
+
+    def __post_init__(self):
+        self.building = get_model(data, Building, self.building)
+        self.floor = get_model(asdict(self.building), Floor, self.floor)
+        
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
+    __key__ ="meetings"
+    __filter__ ="id"
+
+@dataclass
+class DataStore:
+    buildings: list[Building]
+    meetings: list[Meeting]
+    users: list[User]
+
+    def __post_init__(self):
+        self.buildings = [Building(**b) for b in self.buildings]
+        self.meetings = [Meeting(**m) for m in self.meetings]
+        self.users = [User(**u) for u in self.users]
+        
+    def has_user(self, user):
+        return any(u for u in self.users if u.name == user)
+    
+    def add_building(self, building_name, time_slot):
+        self.buildings.append(Building(name=building_name, time_slot=time_slot))
+
+    def __repr__(self) -> str:
+        return json.dumps(asdict(self))
+
+def get_list(data, model):
+    return [model(**d) for d in data[model.__key__]]
+
+def get_model(data, model, key):
+    return next(iter(model(**d) for d in data[model.__key__] if d[model.__filter__] == key), None)
+
+def read_data() -> DataStore:
+    global data
+
+    with open("data.toml", mode="rb") as data_file:
+        data = tomllib.load(data_file)
+
+    return DataStore(**data)
+
+if __name__ == "__main__":
+    read_data()
---
+++ conference-room/run.py
@@ -0,0 +1,76 @@
+from models import DataStore, User, Building, Floor, Meeting, read_data
+
+new_line = "\n"
+tab = "\t"
+space = " "
+
+ds: DataStore = None
+
+def get_item(arr, index, default):
+    return arr[index] if index < len(arr) else default
+
+if __name__ == "__main__":
+    ds = read_data()
+
+    print(f"{new_line}Conference Room Management System{new_line}")
+    username = input("Enter username: ")
+    
+    if not ds.has_user(username):
+        print("user does not exists")
+        username = None
+    
+    while True and username:
+        arg = input("> ")
+        
+        if arg in ["h", "help"]:
+            with open("help.txt") as file_help: print(file_help.read())
+        elif arg in ["d", "dump"]:
+            print(ds)
+        elif arg in ["e", "exit"]:
+            break
+        
+        elif arg == "add":
+            command = input(f"{tab}> ").split(space)
+            
+            if command[0] == "building":
+                if len(command) < 2:
+                    print("invalid number of arguments")
+                else:
+                    building_name = command[1]
+                    time_slot = get_item(command, 2, [10,20])
+                    #add_building(building_name, time_slot)
+    """
+    if username not in user_data:
+        print("user does not exists!!!")
+        username = None
+
+    while True and username:
+        arg = input("> ")
+
+        if arg == "h" or arg == "help":
+            with open("help") as file_help:
+                print(file_help.read())
+        elif arg == "d" or arg == "dump":
+            old_debug, debug = debug, True
+            echo(building_data, preety=True)
+            echo(meeting_data, preety=True)
+            debug = old_debug
+        elif arg == "e" or arg == "exit":
+            break
+        elif arg == "add":
+            command = input(f"{tab}> ").split(space)
+            if command[0] == "building":
+                if len(command) < 2:
+                    print("invalid numbers of arguments")
+                else:
+                   building_name = command[1]
+                   time_slot = get_item(command, 2, [10,20])
+                   add_building(building_name, time_slot)
+            elif command[0] == "floor":
+                if len(command) < 3:
+                    print("invalid numbers of arguments")
+                else:
+                    building_name = command[1]
+                    floor_name = command[2]
+                    add_floor(building_name, floor_name)
+    """
